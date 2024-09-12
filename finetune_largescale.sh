d9f1785f4d44111d28af9cd04119b587ad44ec78
szlbiubiubiu <sunlf281@gmail.com>
2024-09-12 12:18:05 +0100

    update

--- finetune_largescale.sh
+++ finetune_largescale.sh
@@ -3,8 +3,8 @@
 #$ -j y
 #$ -l h_rt=1:0:0     # XX hours runtime
 #$ -l h_vmem=11G      # 11G RAM per core
-#$ -pe smp 16          # 8 cores per GPU
-#$ -l gpu=2           # request 1 GPU
+#$ -pe smp 8          # 8 cores per GPU
+#$ -l gpu=1           # request 1 GPU
 #$ -l gpu_type=ampere   
 ##$ -l cluster=andrena
 ##$ -m se
@@ -18,7 +18,7 @@ conda activate py37
 # pip install -r requirements
 # python -m torch.distributed.launch --nproc_per_node=2 dino_landmark_web.py   #> output.log
 # python -m torch.distributed.launch --nproc_per_node=2 --nnodes=1 --node_rank=0  --master_port 47770 train_webface_dis.py
-python -m torch.distributed.launch --nproc_per_node=2 --nnodes=1 --node_rank=0  --master_port 47771 train_largescale.py \
+python -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0  --master_port 47771 train_largescale.py \
 --pretrain_path /data/home/acw569/precheck/webface_196land_sp/Backbone_VIT_land_8_Epoch_34_Batch_327225_Time_2022-05-05-10-34_checkpoint.pth --model_dir /data/scratch/acw569/checkpoint/ssl_check/SSL_Webface_webland_partViTB.pth \
 --dataset_path /data/scratch/acw569/webface_4m/webface_rec --eval_root /data/scratch/acw569/webface_4m/ms1m-retinaface-t1 \
 --outdir /data/scratch/acw569/checkpoint/sp_check/webface_webland_34epoch_mixup0201_aug01
